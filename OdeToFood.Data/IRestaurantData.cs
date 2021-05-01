5f90b588879e4217984882b67646b8262ccba994
porquet jean <30966472+JFancis@users.noreply.github.com>
2021-05-01 05:54:20 +0000

    Ajoutez des fichiers projet.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+using System.Linq
+
+namespace OdeToFood.Data
+{
+     public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id=1, Name="Jean Pizza",Location="Abidjan", Cuisine=CuisineType.Indian },
+                new Restaurant {Id=1, Name="Jean Pizza",Location="Abidjan", Cuisine=CuisineType.Indian },
+                new Restaurant {Id=1, Name="Jean Pizza",Location="Abidjan", Cuisine=CuisineType.Indian },
+                new Restaurant {Id=1, Name="Jean Pizza",Location="Abidjan", Cuisine=CuisineType.Indian },
+            };
+
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants orderby r.Name
+                   select r; 
+            
+        }
+    }
+}
